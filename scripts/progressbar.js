59abe8146abc94d64ab831d7535f71a9d4318ffd
Stefan Hartmann <hartmann_ste@gmx.de>
2020-10-18 19:56:18 +0200

    added logo

--- scripts/progressbar.js
+++ scripts/progressbar.js
@@ -1,7 +1,7 @@
 // scroll function
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollbar()};
 
-function myFunction() {
+function scrollbar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
